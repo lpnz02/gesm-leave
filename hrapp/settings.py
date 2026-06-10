ae14c4cde393de0b1b820a832d5e2e9a0d2ed920
Lila <lilapanizza2@gmail.com>
2026-06-10 11:22:15 +0800

    settings.py

--- hrapp/settings.py
+++ hrapp/settings.py
@@ -21,43 +21,43 @@ AUTH_USER_MODEL = 'accounts.User'
 # APPLICATIONS
 # -------------------------------------------------------
 INSTALLED_APPS = [
-   'django.contrib.admin',
-   'django.contrib.auth',
-   'django.contrib.contenttypes',
-   'django.contrib.sessions',
-   'django.contrib.messages',
-   'django.contrib.staticfiles',
-   'accounts',
-   'leaves',
-   'notifications',
-   'dashboard',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'accounts',
+    'leaves',
+    'notifications',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
-   'django.middleware.security.SecurityMiddleware',
-   'django.contrib.sessions.middleware.SessionMiddleware',
-   'django.middleware.common.CommonMiddleware',
-   'django.middleware.csrf.CsrfViewMiddleware',
-   'django.contrib.auth.middleware.AuthenticationMiddleware',
-   'django.contrib.messages.middleware.MessageMiddleware',
-   'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'hrapp.urls'
 
 TEMPLATES = [
-   {
-       'BACKEND': 'django.template.backends.django.DjangoTemplates',
-       'DIRS': [BASE_DIR / 'templates'],
-       'APP_DIRS': True,
-       'OPTIONS': {
-           'context_processors': [
-               'django.template.context_processors.request',
-               'django.contrib.auth.context_processors.auth',
-               'django.contrib.messages.context_processors.messages',
-           ],
-       },
-   },
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 WSGI_APPLICATION = 'hrapp.wsgi.application'
@@ -66,24 +66,24 @@ WSGI_APPLICATION = 'hrapp.wsgi.application'
 # DATABASE
 # -------------------------------------------------------
 DATABASES = {
-   'default': {
-       'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-       'NAME': os.getenv('DB_NAME', str(BASE_DIR / 'db.sqlite3')),
-       'USER': os.getenv('DB_USER', ''),
-       'PASSWORD': os.getenv('DB_PASSWORD', ''),
-       'HOST': os.getenv('DB_HOST', ''),
-       'PORT': os.getenv('DB_PORT', ''),
-   }
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DB_NAME', str(BASE_DIR / 'db.sqlite3')),
+        'USER': os.getenv('DB_USER', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', ''),
+    }
 }
 
 # -------------------------------------------------------
 # PASSWORD VALIDATION
 # -------------------------------------------------------
 AUTH_PASSWORD_VALIDATORS = [
-   {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-   {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-   {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-   {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 # -------------------------------------------------------
@@ -100,6 +100,7 @@ USE_TZ = True
 # -------------------------------------------------------
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # ← ajouté : requis pour collectstatic
 
 # -------------------------------------------------------
 # DEFAULT PRIMARY KEY
